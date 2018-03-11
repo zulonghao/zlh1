509b03edd9f10de6bd963b6c740402df72845a23
zulonghao <726461151@qq.com>
2018-03-11 16:48:41 +0800

    输出再修改2

---
+++ src/main/java/com/bdqn/test1/Test2.java
@@ -0,0 +1,10 @@
+package com.bdqn.test1;
+
+/**
+ * Created by zlh on 2018/3/11.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("osdfjoa");
+    }
+}
