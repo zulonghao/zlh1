51be301b0c803b5bff9b5555026bf838d6a78851
zulonghao <726461151@qq.com>
2018-03-11 13:03:13 +0800

    测试,测试

---
+++ src/main/java/com/bdqn/test1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.bdqn.test1;
+
+/**
+ * Created by zlh on 2018/3/11.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
