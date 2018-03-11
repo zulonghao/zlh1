5f2a71da0363f140711f86de08f3c45aade1a36b
zulonghao <726461151@qq.com>
2018-03-11 16:26:15 +0800

    输出再修改

--- src/main/java/com/bdqn/test1/HelloWorld.java
+++ src/main/java/com/bdqn/test1/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
         System.out.println("sdojfosdajfo");
+        System.out.println("hhhhhh");
     }
 }
