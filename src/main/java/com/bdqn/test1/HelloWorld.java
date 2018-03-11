897fcdc4a169fa0d4cecc89bd66e4a01db3b2827
zulonghao <726461151@qq.com>
2018-03-11 16:22:39 +0800

    输出修改

--- src/main/java/com/bdqn/test1/HelloWorld.java
+++ src/main/java/com/bdqn/test1/HelloWorld.java
@@ -6,5 +6,6 @@ package com.bdqn.test1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
+        System.out.println("sdojfosdajfo");
     }
 }
